60c6c4030c6286d00ee2ec73e5866e5571973e2f
Shea Phillips <shea.phillips@gmail.com>
2017-01-07 08:03:22 -0800

    update to detect SSL vs. non-SSL routes and build appropriate URL

--- monitoring/check_urls.py
+++ monitoring/check_urls.py
@@ -1,7 +1,8 @@
 import requests
 
-
+import re
 import fileinput
+import sys
 
 routes = []
 
@@ -20,16 +21,16 @@ for route in routes:
 
     protocol = "http://"
 
+    # pat = re.search('edge|Redirect')
+    if re.search('edge|Redirect', route):
+        protocol = "https://"
+
     extra_cols = 0
     path = route_parts[3]
 
     if "/" in path:
         base_url = base_url + path
         extra_cols = extra_cols+1
-
-    ssl_opt = route_parts[extra_cols+4]
-    if "=" not in ssl_opt:
-        protocol="https://"
 
     full_url = "%s%s" % (protocol, base_url)
 
@@ -40,10 +41,18 @@ for route in routes:
         response = requests.get(full_url)
         response_code = response.status_code
         if response_code not in [200, 403, 404] :
-            print "Failed... project:%s, route: %s, url: %s, response code: %d" % (project, route_name, full_url, response_code )
+            print "\nFailed... project:%s, route: %s, url: %s, response code: %d" % (project, route_name, full_url, response_code )
+            sys.stdout.flush()
             failed.append(full_url)
         else:
+            sys.stdout.write('.')
+            sys.stdout.flush()
             succeeded.append(full_url)
     except Exception as e:
-        print "Gah! project:%s, route: %s, url: %s" % (project, route_name, full_url)
-        print e
+        print "\nFailed (with Exception) ... project:%s, route: %s, url: %s, exception: %s" % (project, route_name, full_url, e )
+
+        # print "\nGah! project:%s, route: %s, url: %s" % (project, route_name, full_url)
+        # print e
+
+    # for successful_route in  succeeded:
+    #     print "Succeed: %s" % (successful_route)
