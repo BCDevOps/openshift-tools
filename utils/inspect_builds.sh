1170212c6a1b865f2496de8da258e5d05be827d3
Shea Phillips <shea.phillips@gmail.com>
2017-01-12 11:53:39 -0800

    - performance improvements - add scc output

--- utils/inspect_builds.sh
+++ utils/inspect_builds.sh
@@ -4,15 +4,18 @@ for project in $(oc get projects --no-headers | awk '{print $1}');
 do
 #    echo "project: $project";
 #    for build in $(oc get builds -n $project --no-headers | awk '/Failed|Cancelled/' | awk '/hours/{print $1}');
-    for build in $(oc get builds -n $project --no-headers  | awk '/hours/{print $1}');
+    for build in $(oc get builds -n $project --no-headers | awk '/hours/{print $1}');
         do
-            build_status=`oc describe build $build -n $project | awk '/Status/{print $2}'`
-            build_started=`oc describe build $build -n $project | awk '/Started/{$1=""; print $0}'`
-            build_duration=`oc describe build $build -n $project | awk '/Duration/{print $2}'`
-            build_created=`oc describe build $build -n $project | awk '/Created/{$1=""; print $0}'`
-            build_pod=`oc describe build $build -n $project | awk '/Build Pod/{print $3}'`
-            build_host=`oc describe pod -n $project $build_pod | awk '/Node/{print $2}'`
+            build_description=$(oc describe build $build -n $project)
+            build_status=`echo "$build_description" | awk '/Status/{print $2}'`
+            build_started=`echo "$build_description" | awk '/Started/{$1=""; print $0}'`
+            build_duration=`echo "$build_description" | awk '/Duration/{print $2}'`
+            build_created=`echo "$build_description" | awk '/Created/{$1=""; print $0}'`
+            build_pod=$(echo "$build_description" | awk '/Build Pod/{print $3}')
+            pod_description=$(oc describe pod -n $project $build_pod)
+            build_host=`echo "$pod_description" | awk '/Node/{print $2}'`
+            security_policy=`echo "$pod_description" | awk '/Security Policy/{print $3}'`
 
-            echo "$project build $build created at $build_created and $build_status in pod $build_pod on host $build_host after $build_duration"
+            echo "$project build $build created at $build_created and $build_status in pod $build_pod on host $build_host with $security_policy after $build_duration"
         done
 done
