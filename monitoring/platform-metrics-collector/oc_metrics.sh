71b606c0df59c5e760c98e8d0d673cd073a979ca
Shea Phillips <shea.phillips@gmail.com>
2017-03-02 15:15:56 -0800

    adding timestamp

--- monitoring/platform-metrics-collector/oc_metrics.sh
+++ monitoring/platform-metrics-collector/oc_metrics.sh
@@ -5,7 +5,7 @@ do
     pod_count=$(oc get pods --no-headers | wc -l)
     echo "Pod count is '$pod_count'"
 
-    curl -i -XPOST 'http://influxdb:8186/write' --data-binary "pod_count,project=myproject value=$pod_count"
+    curl -i -XPOST 'http://influxdb:8186/write' --data-binary "pod_count,project=myproject value=$pod_count $(date +%s)"
 
     sleep 30
 done
