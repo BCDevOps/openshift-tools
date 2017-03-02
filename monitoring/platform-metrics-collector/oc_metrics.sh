de724c1d8c52452a135da744f4c24d96d1b95b5c
Shea Phillips <shea.phillips@gmail.com>
2017-03-02 15:03:45 -0800

    - added curls and fixed oc metrics command

--- monitoring/platform-metrics-collector/oc_metrics.sh
+++ monitoring/platform-metrics-collector/oc_metrics.sh
@@ -2,7 +2,7 @@
 
 while [ 1 ]
 do
-    pod_count=$(oc get pods)
+    pod_count=$(oc get pods --no-headers | wc -l)
     echo "Pod count is '$pod_count'"
 
     curl -i -XPOST 'http://influxdb:8186/write' --data-binary "pod_count,project=myproject value=$pod_count"
