3a9af85fa18a9cd7226e2b41fe16288f0421434e
Shea Phillips <shea.phillips@gmail.com>
2017-03-02 20:32:21 -0800

    convert timestamp to nanoseconds per InfluxDB spec.

--- monitoring/platform-metrics-collector/oc_metrics.sh
+++ monitoring/platform-metrics-collector/oc_metrics.sh
@@ -5,7 +5,10 @@ do
     pod_count=$(oc get pods --no-headers | wc -l)
     echo "Pod count is '$pod_count'"
 
-    curl -i -XPOST "http://${TELEGRAF_HTTP_SERVICE_HOST}:8186/write" --data-binary "pod_count,project=myproject value=${pod_count} $(date +%s)"
+    ts_secs=$(date +%s)
+    ts_nano="$(($ts_secs * 1000000))"
+
+    curl -i -XPOST "http://${TELEGRAF_HTTP_SERVICE_HOST}:8186/write" --data-binary "pod_count,project=myproject value=${pod_count} $ts_nano"
 
     sleep 30
 done
