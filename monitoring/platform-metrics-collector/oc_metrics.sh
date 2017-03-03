fcc2070df9e29da945ecd789a6f252f9824f8335
Shea Phillips <shea.phillips@gmail.com>
2017-03-02 20:58:25 -0800

    change value name

--- monitoring/platform-metrics-collector/oc_metrics.sh
+++ monitoring/platform-metrics-collector/oc_metrics.sh
@@ -7,7 +7,7 @@ do
     ts_secs=$(date +%s)
     ts_nano="$(($ts_secs * 1000000000))"
 
-    payload="pod_count,project=myproject value=${pod_count} $ts_nano"
+    payload="pod_count,project=myproject pods=${pod_count} $ts_nano"
     echo "$payload"
 
     curl -s -i -XPOST "http://${TELEGRAF_HTTP_SERVICE_HOST}:8186/write" --data-binary "$payload"
