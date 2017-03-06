59b2e966e68910cb3470b19e0e6fde8ff7f1fff1
Shea Phillips <shea.phillips@gmail.com>
2017-03-06 14:15:05 -0800

    - look at whole environment

--- monitoring/platform-metrics-collector/oc_metrics.sh
+++ monitoring/platform-metrics-collector/oc_metrics.sh
@@ -2,12 +2,12 @@
 
 while [ 1 ]
 do
-    pod_count=$(oc get pods --no-headers | wc -l | tr -d '[:space:]')
+    pod_count=$(oc get pods --no-headers --all-namespaces | grep Running | wc -l | tr -d '[:space:]')
 
     ts_secs=$(date +%s)
     ts_nano="$(($ts_secs * 1000000000))"
 
-    payload="pod_count,project=myproject pods=${pod_count} $ts_nano"
+    payload="pod_count,scope=cluster pods=${pod_count} $ts_nano"
     echo "$payload"
 
     curl -s -i -XPOST "http://${TELEGRAF_HTTP_SERVICE_HOST}:8186/write" --data-binary "$payload"
