95c1f14e999d819782b992234ded344d2231f4bd
Shea Phillips <shea.phillips@gmail.com>
2017-06-20 11:45:16 -0700

    - explicitly specifying resource limits (above defaults) for sonar container, as it needs a decent amount of RAM to start up and run  - adding dynamic generation of a default path URL for sonar

--- provisioning/sonarqube/deploy-sonarqube.sh
+++ provisioning/sonarqube/deploy-sonarqube.sh
@@ -12,6 +12,15 @@ if [[ -z "${OS_PROJECT_NAME// }" ]]; then
     OS_PROJECT_NAME=$DEFAULT_PROJECT
 fi
 
+DEFAULT_SONAR_ROUTE="$OS_PROJECT_NAME-sonarqube.pathfinder.gov.bc.ca"
+
+echo -n "Enter the URL for the SonarQube  route (or enter for current project '$DEFAULT_SONAR_ROUTE'):"
+    read SONAR_ROUTE
+
+if [[ -z "${SONAR_ROUTE// }" ]]; then
+    SONAR_ROUTE=$DEFAULT_SONAR_ROUTE
+fi
+
 #deploy a postgres instance for sonar to use
 oc new-app postgresql-persistent -p POSTGRESQL_USER=sonarqube,POSTGRESQL_DATABASE=sonarqube -n $OS_PROJECT_NAME
 
@@ -19,7 +28,7 @@ oc new-app postgresql-persistent -p POSTGRESQL_USER=sonarqube,POSTGRESQL_DATABAS
 postgres_password=$(oc env dc postgresql --list | awk  -F  "=" '/POSTGRESQL_PASSWORD/{print $2}')
 
 #deploy sonar in the selected project using the template in the current directory, passing jdbc passsword, etc. as params
-oc process -f sonarqube-deploy.json -v SONARQUBE_JDBC_USERNAME=sonarqube,SONARQUBE_JDBC_PASSWORD=$postgres_password | oc create -n $OS_PROJECT_NAME -f -
+oc process -f sonarqube-deploy.json -v SONARQUBE_ROUTE_URL=$SONAR_ROUTE,SONARQUBE_JDBC_USERNAME=sonarqube,SONARQUBE_JDBC_PASSWORD=$postgres_password | oc create -n $OS_PROJECT_NAME -f -
 
 
 echo "*****IMPORTANT**** Once SonarQube has successfully deployed and initialized (takes a couple minutes), you must run ./update-sonarqube-password.sh to ensure the default admin password is regenerated."
