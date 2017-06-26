a25ddd982ffc6bf889da38f50ca4004d6a89b3e4
Shea Phillips <shea.phillips@gmail.com>
2017-06-26 15:43:16 -0700

    - update jenkinsfile generator, etc.

---
+++ provisioning/README.md
@@ -0,0 +1,20 @@
+#DevOps Platform Onboarding and Quickstart Tools/Process
+
+This document describes the process/steps and tools involved in onboarding a development team onto the BC Gov DevOps platform.
+  
+  
+The general steps are as follows:
+    
+* Request Project Set 
+* Create Project Set
+* Populate "Tools" project space with Jenkins and "Skeleton" Pipeline
+* Apply "Quickstart"
+* Update/Add Pipelines as necessary
+
+Each of these will be discussed in more detail below.
+
+## Request Project Set
+
+
+
+
--- provisioning/pipeline/create-jenkinsfile.sh
+++ provisioning/pipeline/create-jenkinsfile.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-while getopts ":i:d:" opt; do
+usage(){
+	echo "Usage: $0 -i <IMAGESTREAM_NAME> -d <DEPLOYMENT_CONFIG> -t <JENKINSFILE_TEMPLATE (optional)>"
+	exit 1
+}
+
+TEMPLATE="./resources/Jenkinsfile-template.txt"
+
+while getopts ":i:d:t:" opt; do
   case $opt in
     i)
       IMAGESTREAM_NAME=$OPTARG
-      echo "imagestream name is '$IMAGESTREAM_NAME'" >&2
       ;;
     d)
       DEPLOYMENT_CONFIG=$OPTARG
-      echo "OpenShift DeploymentConfiguration is '$DEPLOYMENT_CONFIG'" >&2
+      ;;
+    t)
+      TEMPLATE=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -16,9 +24,21 @@ while getopts ":i:d:" opt; do
   esac
 done
 
+if [ -z "$IMAGESTREAM_NAME" ] || [ -z "$DEPLOYMENT_CONFIG" ] || [ -z "$TEMPLATE" ]
+then
+    echo "Missing required parameters..."
+    usage
+    exit -1
+fi
+
+echo "OpenShift ImageStream name is '$IMAGESTREAM_NAME'" >&2
+echo "OpenShift DeploymentConfiguration is '$DEPLOYMENT_CONFIG'" >&2
+echo "Jenkinsfile template is '$TEMPLATE'" >&2
+
 export IMAGESTREAM_NAME=$IMAGESTREAM_NAME
 export DEPLOYMENT_CONFIG=$DEPLOYMENT_CONFIG
 
-envsubst '${IMAGESTREAM_NAME} ${DEPLOYMENT_CONFIG}' < ./resources/Jenkinsfile-template.txt > ./Jenkinsfile
+envsubst '${IMAGESTREAM_NAME} ${DEPLOYMENT_CONFIG}' < ${TEMPLATE}
+#envsubst '${IMAGESTREAM_NAME} ${DEPLOYMENT_CONFIG}' < ${TEMPLATE} > ./Jenkinsfile
 
 echo "A Jenkinsfile has been created for you in this directory. You can place this in your GitHub repo and point to it when you run 'create-pipeline.sh'"
