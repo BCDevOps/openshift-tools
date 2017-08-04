2cbc9555e5d3579bf0a3ba75739fce7a808f3a1e
stewartshea <shea.stewart@arctiq.ca>
2017-08-04 09:37:50 -0700

    modified the projects deployed and added a timer, extended sleep 600

--- test/ose_genload.sh
+++ test/ose_genload.sh
@@ -1,23 +1,31 @@
-## A simple application to generate a set amount of projects, builderapps, ad instantapps. Use external monitoring tools while the script executes. 
+## A simple application to generate a set amount of projects, builderapps, ad instantapps. Use external monitoring tools while the script executes.
 #!/bin/bash
-Projects=10
-BuilderApps=5
-InstantApps=10
+Projects=1
+BuilderApps=1
+InstantApps=1
 NOW=$(date +"%m-%d-%Y-%H-%M-%S")
-
+start=`date +%s`
 ## Create projects and applications
 for ((i=0; i<=Projects; i++)); do
    oc new-project "loadtest-00"$NOW-$i
    for ((n=0; n<=BuilderApps; n++)); do
-   	oc new-app https://github.com/openshift/rails-ex.git --name=rails00$n
+   	oc new-app rails-postgresql-example --name=rails00$n
    done
    for ((l=0; l<=InstantApps; l++)); do
-   	oc new-app --image-stream=php:latest --name=php-00$l
+   	oc new-app https://github.com/openshift/cakephp-ex/ --name=php-00$l
    done
 done
+for ((i=0; i<=Projects; i++)); do
+   oc project "loadtest-00"$NOW-$i
+   oc get pods
+done
 
-sleep 60
+sleep 600
 
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Total runtime = "$runtime
 ## Cleanup
 for ((i=0; i<=Projects; i++)); do
    oc delete project "loadtest-00"$NOW-$i
