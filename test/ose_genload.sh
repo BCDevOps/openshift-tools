3205fc92876fb88beaef3bd105eb90a399e103ef
stewartshea <shea.stewart@arctiq.ca>
2017-08-08 19:08:18 -0400

    removing instant apps

--- test/ose_genload.sh
+++ test/ose_genload.sh
@@ -2,7 +2,6 @@
 #!/bin/bash
 Projects=1
 BuilderApps=1
-InstantApps=1
 NOW=$(date +"%m-%d-%Y-%H-%M-%S")
 start=`date +%s`
 ## Create projects and applications
@@ -11,19 +10,18 @@ for ((i=0; i<=Projects; i++)); do
    for ((n=0; n<=BuilderApps; n++)); do
    	oc new-app rails-postgresql-example --name=rails00$n
    done
-   for ((l=0; l<=InstantApps; l++)); do
-   	oc new-app https://github.com/openshift/cakephp-ex/ --name=php-00$l
-   done
 done
 for ((i=0; i<=Projects; i++)); do
    oc project "loadtest-00"$NOW-$i
    oc get pods
 done
 
-sleep 600
 
 end=`date +%s`
 runtime=$((end-start))
+
+sleep 600
+
 
 echo "Total runtime = "$runtime
 ## Cleanup
