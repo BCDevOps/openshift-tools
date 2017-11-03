5a64dabbe42fb7a11eaeb2b868908e8fe624dd3c
Angelika Ehlers <angelika.ehlers@gov.bc.ca>
2017-11-03 13:58:54 -0700

    Add a PVC Template file with varibles

--- utils/storage-migrator/collect_config.sh
+++ utils/storage-migrator/collect_config.sh
@@ -5,12 +5,19 @@ do
   if [ $x != ocp-storage-migrator ]
   then
       oc get dc/$x
+      cn=$(oc describe dc/$x | grep ClaimName | awk '{print $2}')
+      for pvcname in $cn 
+      do 
+        oc get pvc/$pvcname
+        oc export pvc/$pvcname -o yaml --as-template=${pvcname}_old >PVC_$x_$pvcname.yaml
+      done 
   else
       echo "This is us"
   fi
+  echo "----"
 done
 
 
 ##
-# oc project -q
 ## oc export pvc mariadb -o yaml  --as-template=mariadb_old  | sed '/^    volumeName/ d' | sed '/  status/ d' | sed -i 
+# oc project -q
