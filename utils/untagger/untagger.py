09137188d2683a6415daceb06be48cef7848ac23
Shea Phillips <shea.phillips@gmail.com>
2017-12-13 16:47:47 -0800

    - added a little usage snippet.

--- utils/untagger/untagger.py
+++ utils/untagger/untagger.py
@@ -1,5 +1,8 @@
 import sys, re, subprocess
 
+# usage example:
+# oc get istag | awk '/pdf/ { print $1}' | python untagger.py pdf:13 pdf:14 pdf:15 pdf:16 pdf:17 pdf:74 pdf:latest pdf:dev pdf:test pdf:prod
+# ^^^^^^ above would remove all the imagestream tags from the `pdf` imagestream *except* those provided as the last set of arguments.
 
 if __name__ == '__main__':
     to_untag = sys.argv[1:]
