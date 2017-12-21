f659b51bac5d71ebf8895ecebc7346cfc7b300b3
Hawthorne <sehawtho@ociopf-d-100.dmz>
2017-12-21 11:28:49 -0800

    Updated weather report, tracks more disk info, rearranged to look nicer

--- monitoring/wr.sh
+++ monitoring/wr.sh
@@ -12,13 +12,15 @@ function resources {
 	for n in ${@}
 	do
 		NODES="$NODES \"$n\""
-        	CLOAD=$(ssh -q $n "awk '{print \$1, \$2, \$3}' /proc/loadavg")
-        	CVOLS=$(ssh -q $n "df -h | grep openshift.local.volumes | wc -l")
+        	CLOAD=$(ssh -q ocpadmin@$n "awk '{print \$1, \$2, \$3}' /proc/loadavg")
+        	CVOLS=$(ssh -q ocpadmin@$n "sudo df -h | grep openshift.local.volumes | wc -l")
 		CVOLS_PAD=$(echo $CVOLS | awk '{printf "%3s",$1}')
-        	CMETA=$(ssh -q $n "lvdisplay docker-vg/docker-pool | grep 'Allocated metadata'")
+        	CSIZE=$(ssh -q ocpadmin@$n "sudo lvdisplay docker-vg/docker-pool | grep 'Allocated pool data'")
+		CSIZE_PAD=$(echo $CSIZE | sed -e 's/%//' | awk '{printf "%2.f",$4}')
+        	CMETA=$(ssh -q ocpadmin@$n "sudo lvdisplay docker-vg/docker-pool | grep 'Allocated metadata'")
 		CMETA_PAD=$(echo $CMETA | sed -e 's/%//' | awk '{printf "%2.f",$3}')
-        	CKUBE=$(oc describe node $n | egrep -A 4 'Name:|Allocated resources:|Kubelet Version:' | egrep 'Name:|%|Non-terminated Pods' | sed -e 's/Non-terminated Pods://g' -e 's/Name://g' | perl -p -i -e 's/dmz\n/dmz /g' | perl -p -i -e 's/in total\)\n/ /g' | tr -d '()' | awk '{printf "Node: %s  Pods: %2s  CPU: %3s  Memory: %3s", $1,$2,$4,$8}' | perl -p -i -e 's/\n//')
-        	MSG="$MSG $CKUBE  Volumes: $CVOLS_PAD  Metadata: $CMETA_PAD%  Load: $CLOAD \n"
+        	CKUBE=$(oc describe node $n | egrep -A 4 'Name:|Allocated resources:|Kubelet Version:' | egrep 'Name:|%|Non-terminated Pods' | sed -e 's/Non-terminated Pods://g' -e 's/Name://g' | perl -p -i -e 's/dmz\n/dmz /g' | perl -p -i -e 's/in total\)\n/ /g' | tr -d '()' | awk '{printf "Node: %s  Pods: %2s  CPU: %3s  Mem: %3s", $1,$2,$4,$8}' | perl -p -i -e 's/\n//')
+        	MSG="$MSG $CKUBE Vol: $CVOLS_PAD Data: $CSIZE_PAD% Meta: $CMETA_PAD% Load: $CLOAD \n"
 	done
 
 	NSTAT=$(oc get nodes -o go-template="{{range .items}}{{if  eq .metadata.name $NODES}}{{.metadata.name}}{{print \"\t\"}}{{.status.allocatable.cpu}}{{print \"\t\"}}{{.status.allocatable.memory}}{{print \"\n\"}}{{end}}{{end}}")
@@ -42,7 +44,7 @@ function resources {
 
 function post2slack {
 slackUrl="https://hooks.slack.com/services/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"
-SLACKMSG=$( echo "$MSG" | sed 's/\\/\\\\/g')
+SLACKMSG=$( echo "\n$MSG\n" | sed 's/\\/\\\\/g')
 SLACKMSG="\`\`\`$SLACKMSG\`\`\`"
 
 read -d '' payLoad << EOF
@@ -89,6 +91,46 @@ if [ -n "$QNODES" ]; then
 	resources $QNODES
 fi
 
+MSG="$MSG\nDocker Registry:\n"
+POD=$(oc -n default get pods -l deploymentconfig=docker-registry -o name | sed 's#pods/##' | head -n 1)
+DR=$(oc -n default exec $POD -- df -h /registry)
+MSG="$MSG\n$DR\n"
+
+MSG="$MSG\nMetrics:\n"
+POD=$(oc -n openshift-infra get pods -l type=hawkular-cassandra -o name | sed 's#pods/##' | head -n 1)
+MT=$(oc -n openshift-infra exec $POD -- df -h /cassandra_data)
+MSG="$MSG\n$MT\n"
+
+MSG="$MSG\nTop 10 PVs:\n"
+DISK=$(ssh -q ociopf-d-140.dmz "df -h" | grep vguserspace01 | /bin/sort -n -r -k 5 | head -10 | awk '{print $6}'| awk -F '[/]' '{print $4}' )
+MSG="$MSG\nPV \t\t USED \t PROJ \t\t\t ADMIN"
+for D in $DISK
+do
+PV=$( echo $D | tr _ - | tr [A-Z] [a-z] )
+USED=$(ssh -q ociopf-d-140.dmz "df -h" | grep $D | awk '{print $5}' )
+PROJPOD=$(oc get pv $PV | tail -1 | awk '{print $6}')
+PROJ=$(echo $PROJPOD | awk -F '[/]' '{print $1}' )
+ADMIN=$(oc describe policyBindings -n $PROJ | grep -A2 "RoleBinding\[admin\]" | tail -1 | sed 's/Users://' | tr -d '\t' )
+MSG="$MSG\n$PV \t $USED \t\t $PROJPOD \t $ADMIN"
+done
+
+MSG="$MSG\n\nLegacy Gluster:\n"
+DSPACE=$(ssh -q ocpadmin@ociopf-d-140.dmz "sudo lvdisplay vgdockerspace01 | head -13 | tail -3")
+MSG="$MSG\nDocker Pool:\n"
+MSG="$MSG$DSPACE\n"
+
+USPACE=$(ssh -q ocpadmin@ociopf-d-140.dmz "sudo lvdisplay vguserspace01 | head -13 | tail -3")
+MSG="$MSG\nUser Pool:\n"
+MSG="$MSG$USPACE\n"
+
+MSG="$MSG\nPVs Available:\n"
+for PV in $(oc get pv | awk '{print $2}' | grep Gi | sort | uniq)
+do
+  PVAVAIL=$(oc get pv | grep Available | grep $PV | wc -l)
+  PVTOTAL=$(oc get pv | grep $PV | wc -l)
+  MSG="$MSG$PV $PVAVAIL / $PVTOTAL \n" 
+done
+
 NBUILD=$(oc get builds --all-namespaces -o go-template='{{range .items}}{{if or (eq .status.phase "New") (eq .status.phase "Pending")}}{{.metadata.namespace}}/{{.metadata.name}} {{.metadata.creationTimestamp}}{{print "\n"}}{{end}}{{end}}')
 
 if [ ${#NBUILD} != 0 ]; then
@@ -102,6 +144,8 @@ if [ ${#OPODS} != 0 ]; then
 	MSG="$MSG\nOrphan Pods:\n"
 	MSG="$MSG$OPODS\n"
 fi
+
+
 
 if [[ $1 == "" ]]
 then
