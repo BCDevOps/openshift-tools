1f5e7b1c6dd918a8c0f1aee5c5728116f861716d
Angelika Ehlers <angelika.ehlers@gov.bc.ca>
2018-01-09 16:20:14 -0800

    Update cns_quota_patching.sh

--- pv_scripts/cns_quota_patching.sh
+++ pv_scripts/cns_quota_patching.sh
@@ -8,6 +8,6 @@ do
   Q=$(oc -n $P get quota -o name);
   if [ -n "$Q" ]
   then
-    oc -n $P patch $Q -p '{"spec":{"hard":{"gluster-file.storageclass.storage.k8s.io/persistentvolumeclaims": 0, "gluster-file.storageclass.storage.k8s.io/requests.storage": 0, "gluster-block.storageclass.storage.k8s.io/persistentvolumeclaims": 0, "gluster-block.storageclass.storage.k8s.io/requests.storage": 0}}}';
+    oc -n $P patch $Q -p '{"spec":{"hard":{"gluster-file.storageclass.storage.k8s.io/persistentvolumeclaims": 10, "gluster-file.storageclass.storage.k8s.io/requests.storage": 200Gi, "gluster-block.storageclass.storage.k8s.io/persistentvolumeclaims": 5, "gluster-block.storageclass.storage.k8s.io/requests.storage": 200Gi}}}';
   fi
 done
