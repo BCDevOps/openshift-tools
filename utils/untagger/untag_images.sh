f3d538d4c706ec73113b5238eeb50e898d556bd0
Wade Barnes <wade.barnes@shaw.ca>
2018-07-05 11:45:24 -0700

    Fix untagger script

--- utils/untagger/untag_images.sh
+++ utils/untagger/untag_images.sh
@@ -3,7 +3,7 @@ source untag_images_usage.inc
 source untag_images_args_check.inc
 #echo "Untagging images $IMAGE_NAME"
 #echo "$DAYS_TO_KEEP"
-cutOffDate=`date -v -${DAYS_TO_KEEP}d +%Y%m%d`
+cutOffDate=$(date -d "-${DAYS_TO_KEEP} days" +%Y%m%d)
 echo "IMAGE NAME:   $IMAGE_NAME"
 echo "DAYS TO KEEP: $DAYS_TO_KEEP   (i.e. images created before $cutOffDate will be untagged!)"
 echo -n "Parameter values correct? (y/n):"
@@ -19,15 +19,13 @@ imageTagPrefix="${IMAGE_NAME}:"
 myNoFormat='^[0-9]+$'
 for isTag in $imageTagList
 do
-  if [[ "$isTag" == "${IMAGE_NAME}:"* ]]
-  then 
+  if [[ "$isTag" == "${IMAGE_NAME}:"* ]]; then
     buildNo=${isTag#$imageTagPrefix}
-    if [[ $buildNo =~ $myNoFormat ]] ; then
+    if [[ $buildNo =~ $myNoFormat ]]; then
       imageCreationTime=`oc get istag $isTag -o template --template="{{.metadata.creationTimestamp}}"`
       imageDate=${imageCreationTime%T*}
       imgDate=`echo "$imageDate"|sed 's/\-//g'`
-      if [ $imgDate -lt $cutOffDate ]
-      then
+      if [[ $imgDate -lt $cutOffDate ]]; then
         echo "UNTAG: $isTag -- created  $imgDate "
         oc tag -d $isTag
       fi
