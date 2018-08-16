9f6598b02d42c51c019c67ea1ac3f219b7214d87
Shea Phillips <shea.phillips@gmail.com>
2018-08-16 13:53:25 -0700

    adding harness for billing collector

---
+++ provisioning/repository/add_user_to_org.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# provide a variable to write to as $1, prompt text as $2 and a default value for $3
+function promptForValue {
+    local __resultvar=$1
+    local __default=$3
+
+    echo -n "$2"
+    read VALUE
+
+    if [[ -z "${VALUE// }" ]]; then
+        eval $__resultvar="'$__default'"
+    else
+        eval $__resultvar="'$VALUE'"
+    fi
+}
+
+promptForValue ORG_NAME  "Organization name: " ${ORG_NAME}
+promptForValue GITHUB_USER_NAME  "GitHub username: " ${GITHUB_USER_NAME}
+promptForValue GITHUB_TOKEN "GitHub token: " ${GITHUB_TOKEN}
+
+GITHUB_API="https://api.github.com/orgs/${ORG_NAME}/memberships/${GITHUB_USER_NAME}"
+
+#echo ${GITHUB_API}
+
+curl -v -X PUT -i -H "Authorization: token ${GITHUB_TOKEN}" ${GITHUB_API}
