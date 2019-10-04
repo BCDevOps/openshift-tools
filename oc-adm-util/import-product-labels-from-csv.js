f68cd7ec569e480c09557fcfc28ad59bde234fce
ShellyXueHan <shelly.han@gov.bc.ca>
2019-10-04 10:12:44 -0700

    added backup before updaing project labels

--- oc-adm-util/import-product-labels-from-csv.js
+++ oc-adm-util/import-product-labels-from-csv.js
@@ -6,36 +6,68 @@
 
 const fs = require('fs');
 const parse = require('csv-parse');
+const { spawnSync } = require('child_process');
 
 // Create the parser
 const parser = parse({ delimiter: ',', columns: true });
 
 // Skip labels/annotation when applying changes:
-const ignoreFields = ['namespace', 'product-owner', 'product-lead'];
+const ignoreFields = [
+  'namespaces',
+  'environments',
+  'product-owner',
+  'product-lead'
+];
 
 // Use the readable stream api
 parser.on('readable', function() {
   let record;
   while ((record = parser.read())) {
-    const args = [
+    const projectSet = record.namespaces.split(',');
+    const projectName = record.name;
+
+    // backup:
+    const getProjectArgs = [
       '--as=system:serviceaccount:openshift:bcdevops-admin',
-      'label',
-      `namespace/${record.namespace}`,
-      '--overwrite'
+      'get',
+      `namespace/${projectSet[0]}`,
+      '-o',
+      'json'
     ];
+    const proc = spawnSync('oc', getProjectArgs);
+    fs.writeFileSync(`output/backup-${projectName}.json`, proc.stdout);
+
+    // update:
+    const labelArgs = [];
     for (let label of Object.keys(record)) {
       if (!ignoreFields.includes(label)) {
         /** @type {string} */
         const value = record[label];
         if (value.length > 0) {
-          args.push(`${label}=${record[label]}`);
+          labelArgs.push(`${label}=${record[label]}`);
         } else {
-          args.push(`${label}-`);
+          labelArgs.push(`${label}-`);
         }
       }
     }
-    console.dir(args);
+
+    projectSet.forEach(namespace => {
+      const args = [
+        '--as=system:serviceaccount:openshift:bcdevops-admin',
+        'label',
+        `namespace/${namespace}`,
+        '--overwrite'
+      ];
+
+      const fullArgs = args.concat(labelArgs);
+      console.dir(fullArgs);
+      const proc = spawnSync('oc', fullArgs);
+      if (proc.stderr) {
+        console.log(Error(proc.stderr));
+      }
+      console.log(`Info: ${proc.stdout}`);
+    });
   }
 });
 
-fs.createReadStream(__dirname + '/output/projects.csv').pipe(parser);
+fs.createReadStream(__dirname + '/output/products.csv').pipe(parser);
